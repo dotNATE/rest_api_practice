83b2c12d9f18b6434507e03fcd4ee70ed7594b1b
dotNate <somersetnathaniel@gmail.com>
2022-10-09 15:49:59 +0100

    added a projects endpoint

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Project struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Link        string `json:"link"`
+}
+
+type Projects []Project
+
+func allProjects(w http.ResponseWriter, r *http.Request) {
+	projects := Projects{
+		Project{Title: "Example project", Description: "This is a description", Link: "https://github.com/dotNATE/rest_api_practice"},
+	}
+
+	fmt.Println("Endpoint hit: All projects endpoint")
+	json.NewEncoder(w).Encode(projects)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage endpoint hit")
@@ -12,6 +30,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/projects", allProjects)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
